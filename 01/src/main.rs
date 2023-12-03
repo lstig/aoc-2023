8250ca9922f885367d7e2ddcc23ed36917c3bd4b
Luke Stigdon <lstig@users.noreply.github.com>
2023-12-03 15:48:10 -0700

    day 1, part 2

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,3 +1,6 @@
+use aho_corasick::AhoCorasick;
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("input.txt");
 
@@ -26,8 +29,42 @@ fn part1(input: &str) -> u32 {
     sum
 }
 
+const NUMS: [(&'static str, &'static str); 10] = [
+    ("zero", "0"),
+    ("one", "1"),
+    ("two", "2"),
+    ("three", "3"),
+    ("four", "4"),
+    ("five", "5"),
+    ("six", "6"),
+    ("seven", "7"),
+    ("eight", "8"),
+    ("nine", "9"),
+];
+
 fn part2(input: &str) -> u32 {
-    todo!()
+    let mut sum = 0;
+    let nummap: HashMap<_, _> = NUMS.into_iter().collect();
+    let patterns = &[
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "one","two","three","four","five","six","seven","eight","nine"
+    ];
+    let re = AhoCorasick::new(patterns).unwrap();
+    for line in input.lines() {
+        let mut value = String::from("");
+        let matches: Vec<_> = re.find_overlapping_iter(line).map(|m| { m.pattern() }).collect();
+
+        for id in vec![matches[0], matches[matches.len() - 1]] {
+            let m = patterns[id.as_usize()];
+            if nummap.contains_key(m) {
+                value.push_str(nummap[m]);
+            } else {
+                value.push_str(m);
+            }
+        }
+        sum += value.parse::<u32>().unwrap();
+    }
+    sum
 }
 
 #[cfg(test)]

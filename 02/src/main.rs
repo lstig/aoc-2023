ad4fc386c8e58ac642e2350ff0b18c103b9313d4
Luke Stigdon <lstig@users.noreply.github.com>
2023-12-03 15:52:50 -0700

    day 2

---
+++ 02/src/main.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> u32 {
+    todo!()
+}
+
+fn part2(input: &str) -> u32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn check_part1() {
+        let input = indoc! {"
+            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+        };
+        let result = part1(input);
+        assert_eq!(result, 8)
+    }
+
+    #[test]
+    fn check_part2() {
+        let input = indoc! {""};
+        let result = part2(input);
+        assert_eq!(result, 0)
+    }
+}

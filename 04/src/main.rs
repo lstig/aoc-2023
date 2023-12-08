111d9b0ed51c7f376152df9902296095b97d371f
Luke Stigdon <lstig@users.noreply.github.com>
2023-12-07 19:22:09 -0700

    day 4, part 2

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let input = include_str!("input.txt");
@@ -8,39 +8,77 @@ fn main() {
     println!("Part 2: {}", part2(input));
 }
 
-fn part1(input: &str) -> i32 {
-    let mut sum: i32 = 0;
+fn score_card(input: &str) -> (u32, i32) {
+    let mut wins: Option<i32> = None;
     let re = Regex::new(r":\s+(.+)\s+\|\s+(.+)").unwrap();
-    for line in input.lines() {
-        let mut wins: Option<i32> = None;
-        let caps = re.captures(line).unwrap();
-        let winners: HashSet<i32> = HashSet::from_iter(
-            caps.get(1)
-                .map_or("", |m| m.as_str())
-                .split_whitespace()
-                .map(|m| m.parse::<i32>().unwrap()),
-        );
-        let picks = caps
-            .get(2)
+    let caps = re.captures(input).unwrap();
+    let winners: HashSet<i32> = HashSet::from_iter(
+        caps.get(1)
             .map_or("", |m| m.as_str())
             .split_whitespace()
-            .map(|m| m.parse::<i32>().unwrap());
+            .map(|m| m.parse::<i32>().unwrap()),
+    );
+    let picks = caps
+        .get(2)
+        .map_or("", |m| m.as_str())
+        .split_whitespace()
+        .map(|m| m.parse::<i32>().unwrap());
 
-        for pick in picks {
-            if winners.contains(&pick) {
-                wins = Some(wins.unwrap_or(-1) + 1);
-            }
-        }
 
-        if wins.is_some() {
-            sum += 2_i32.pow(wins.unwrap() as u32)
+    for pick in picks {
+        if winners.contains(&pick) {
+            wins = Some(wins.unwrap_or(-1) + 1);
         }
     }
+
+    if wins.is_some() {
+        (wins.unwrap() as u32 + 1, 2_i32.pow(wins.unwrap() as u32))
+    } else {
+        (0, 0)
+    }
+}
+
+fn part1(input: &str) -> i32 {
+    let mut sum: i32 = 0;
+
+    for line in input.lines() {
+        let (_, score) = score_card(line);
+        sum += score;
+    }
+
     sum
 }
 
-fn part2(input: &str) -> i32 {
-    todo!()
+fn count_cards(cards: &Vec<&str>, visited: &mut HashMap<usize, u32>, card: &str, i: usize) -> u32 {
+    if let Some(count) = visited.get(&i) {
+        return *count
+    }
+
+    let (count, _) = score_card(card);
+    let mut sum = count;
+
+    let start = i + 1;
+    for j in start..start + count as usize {
+        sum += count_cards(cards, visited, cards[j], j);
+    }
+
+    visited.insert(i, sum);
+
+    sum
+}
+
+fn part2(input: &str) -> u32 {
+    let mut sum = 0;
+
+    let cards: Vec<&str> = input.lines().collect();
+    let mut visited: HashMap<usize, u32> = HashMap::new();
+
+    for (i, card) in cards.iter().enumerate() {
+        sum += 1;
+        sum += count_cards(&cards, &mut visited, card, i);
+    }
+
+    sum
 }
 
 #[cfg(test)]

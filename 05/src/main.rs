9e47a6c481c3515b510a028dd6313be9a753be5b
Luke Stigdon <lstig@users.noreply.github.com>
2023-12-08 08:52:53 -0700

    day 5

---
+++ 05/src/main.rs
@@ -0,0 +1,68 @@
+fn main() {
+    let input = include_str!("input.txt");
+
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> i32 {
+    todo!()
+}
+
+fn part2(input: &str) -> u32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn check_part1() {
+        let input = indoc! {"
+            seeds: 79 14 55 13
+
+            seed-to-soil map:
+            50 98 2
+            52 50 48
+
+            soil-to-fertilizer map:
+            0 15 37
+            37 52 2
+            39 0 15
+
+            fertilizer-to-water map:
+            49 53 8
+            0 11 42
+            42 0 7
+            57 7 4
+
+            water-to-light map:
+            88 18 7
+            18 25 70
+
+            light-to-temperature map:
+            45 77 23
+            81 45 19
+            68 64 13
+
+            temperature-to-humidity map:
+            0 69 1
+            1 0 69
+
+            humidity-to-location map:
+            60 56 37
+            56 93 4"
+        };
+        let result = part1(input);
+        assert_eq!(result, 35)
+    }
+
+    #[test]
+    fn check_part2() {
+        let input = indoc! {""};
+        let result = part2(input);
+        assert_eq!(result, 30)
+    }
+}

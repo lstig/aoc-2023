1e45417a8bc63c06f7e1bd2f08c568269f679ac1
Luke Stigdon <lstig@users.noreply.github.com>
2023-12-13 15:15:59 -0700

    day 6, part 1

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -5,8 +5,40 @@ fn main() {
     println!("Part 2: {}", part2(input));
 }
 
-fn part1(_input: &str) -> u32 {
-    todo!()
+fn parse_input(input: &str) -> Vec<(u32, u32)> {
+    let time: Vec<_> = input
+        .lines()
+        .nth(0) // first line contains Time
+        .unwrap()
+        .split_whitespace()
+        .skip(1) // skip the line label
+        .map(|n| n.parse::<u32>().unwrap())
+        .collect();
+    let distance: Vec<_> = input
+        .lines()
+        .nth(1) // second line contains Distance
+        .unwrap()
+        .split_whitespace()
+        .skip(1) // skip the line label
+        .map(|n| n.parse::<u32>().unwrap())
+        .collect();
+
+    // return tuples of (time, distance)
+    time.into_iter().zip(distance.into_iter()).collect()
+}
+
+fn part1(input: &str) -> u32 {
+    let races = parse_input(input);
+    let mut wins = Vec::new();
+    for (i, (time, distance)) in races.into_iter().enumerate() {
+        wins.push(0);
+        for (hold, accelerate) in (0..=time).rev().enumerate() {
+            if hold as u32 * accelerate > distance {
+                wins[i] += 1;
+            }
+        }
+    }
+    wins.into_iter().reduce(|acc, e| acc * e).unwrap()
 }
 
 fn part2(_input: &str) -> u32 {
@@ -19,8 +51,8 @@ mod tests {
     use indoc::indoc;
 
     static INPUT: &str = indoc! {"
-            Time:      7  15   30
-            Distance:  9  40  200"
+        Time:      7  15   30
+        Distance:  9  40  200"
     };
 
     #[test]

961b3e9d028f1bc9c162bbe1a519f85f8afcdf88
Luke Stigdon <lstig@users.noreply.github.com>
2023-12-14 19:16:26 -0700

    day 8, part 1

--- 08/src/main.rs
+++ 08/src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::{HashMap, VecDeque};
+use regex::Regex;
+
 fn main() {
     let input = include_str!("input.txt");
 
@@ -6,10 +9,46 @@ fn main() {
 }
 
 fn part1(input: &str) -> u32 {
-    0
+    // sections are separated by empty lines
+    let re = Regex::new(r"(?m)\n\n").unwrap();
+    let mut iter = re.split(input);
+    let mut directions = VecDeque::from_iter(iter.next().unwrap().chars());
+
+    let mut tree = HashMap::new();
+    let re = Regex::new(r"([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)").unwrap();
+    for line in iter.next().unwrap().lines() {
+        let caps = re.captures(line).unwrap();
+        let (key, (left, right)) = (
+            caps.get(1).unwrap().as_str(),
+            (
+                caps.get(2).unwrap().as_str(),
+                caps.get(3).unwrap().as_str(),
+            ),
+        );
+        tree.insert(key, (left, right));
+    }
+
+    let mut steps = 0;
+    let mut current = "AAA";
+    while current != "ZZZ" {
+        // get current direction
+        let direction = directions.pop_front().unwrap();
+        directions.push_back(direction);
+
+        // increase steps taken
+        steps += 1;
+
+        // update current node with the next in the tree based on the directions
+        current = match direction {
+            'L' => { tree.get(&current).unwrap().0 },
+            'R' => { tree.get(&current).unwrap().1 },
+            _ => panic!(), // this should never happen
+        };
+    }
+    steps
 }
 
-fn part2(input: &str) -> u32 {
+fn part2(_input: &str) -> u32 {
     0
 }
 
@@ -18,23 +57,22 @@ mod tests {
     use super::*;
     use indoc::indoc;
 
-    static INPUT: &str = indoc! {"
-        LLR
-
-        AAA = (BBB, BBB)
-        BBB = (AAA, ZZZ)
-        ZZZ = (ZZZ, ZZZ)"
-    };
-
     #[test]
     fn check_part1() {
-        let result = part1(INPUT);
-        assert_eq!(result, 3)
+        let input = indoc! {"
+            LLR
+
+            AAA = (BBB, BBB)
+            BBB = (AAA, ZZZ)
+            ZZZ = (ZZZ, ZZZ)"
+        };
+        let result = part1(input);
+        assert_eq!(result, 6)
     }
 
     #[test]
     fn check_part2() {
-        let result = part2(INPUT);
+        let result = part2("");
         assert_eq!(result, 0)
     }
 }

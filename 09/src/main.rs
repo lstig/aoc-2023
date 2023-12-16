1996e45a443b420bcdac95ff35ab06cd6e3ba4b8
Luke Stigdon <lstig@users.noreply.github.com>
2023-12-15 21:30:32 -0700

    refactor

--- 09/src/main.rs
+++ 09/src/main.rs
@@ -6,29 +6,16 @@ fn main() {
 }
 
 // Given a vector of numbers, returns the next number in the current line
-fn next_number(numbers: Vec<i32>) -> i32 {
+fn next_number(numbers: Vec<i32>, f: fn(Vec<i32>, i32) -> i32) -> i32 {
     // If the line is all zeros, we've hit the bottom and should return.
     if numbers.iter().all(|e| *e == 0) {
         return 0;
     }
 
     // Get the next number from the line after ours so we can calculate our next number
-    let next_child = next_number(numbers.windows(2).map(|e| e[1] - e[0]).collect());
+    let next_child = next_number(numbers.windows(2).map(|e| e[1] - e[0]).collect(), f);
 
-    return numbers[numbers.len() - 1] + next_child;
-}
-
-// Given a vector of numbers, returns the next number in the current line
-fn previous_number(numbers: Vec<i32>) -> i32 {
-    // If the line is all zeros, we've hit the bottom and should return.
-    if numbers.iter().all(|e| *e == 0) {
-        return 0;
-    }
-
-    // Get the previous number from the line after ours so we can calculate our next number
-    let previous_child = previous_number(numbers.windows(2).map(|e| e[1] - e[0]).collect());
-
-    return numbers[0] - previous_child;
+    return f(numbers, next_child);
 }
 
 fn part1(input: &str) -> i32 {
@@ -38,6 +25,7 @@ fn part1(input: &str) -> i32 {
             line.split_whitespace()
                 .map(|e| e.parse::<i32>().unwrap())
                 .collect(),
+            |a, b| a[a.len() - 1] + b,
         )
     }
     sum
@@ -46,10 +34,11 @@ fn part1(input: &str) -> i32 {
 fn part2(input: &str) -> i32 {
     let mut sum = 0;
     for line in input.lines() {
-        sum += previous_number(
+        sum += next_number(
             line.split_whitespace()
                 .map(|e| e.parse::<i32>().unwrap())
                 .collect(),
+            |a, b| a[0] - b,
         );
     }
     sum

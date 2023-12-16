b5a63c34ef6a680916c7715d72a2f7bf73e86b7b
Luke Stigdon <lstig@users.noreply.github.com>
2023-12-15 20:55:01 -0700

    day 9, part 1 and part 2

--- 09/src/main.rs
+++ 09/src/main.rs
@@ -5,12 +5,54 @@ fn main() {
     println!("Part 2: {}", part2(input));
 }
 
-fn part1(_input: &str) -> i32 {
-    0
+// Given a vector of numbers, returns the next number in the current line
+fn next_number(numbers: Vec<i32>) -> i32 {
+    // If the line is all zeros, we've hit the bottom and should return.
+    if numbers.iter().all(|e| *e == 0) {
+        return 0;
+    }
+
+    // Get the next number from the line after ours so we can calculate our next number
+    let next_child = next_number(numbers.windows(2).map(|e| e[0] - e[1]).collect());
+
+    return numbers[numbers.len() - 1] - next_child;
 }
 
-fn part2(_input: &str) -> i32 {
-    0
+// Given a vector of numbers, returns the next number in the current line
+fn previous_number(numbers: Vec<i32>) -> i32 {
+    // If the line is all zeros, we've hit the bottom and should return.
+    if numbers.iter().all(|e| *e == 0) {
+        return 0;
+    }
+
+    // Get the previous number from the line after ours so we can calculate our next number
+    let previous_child = previous_number(numbers.windows(2).map(|e| e[1] - e[0]).collect());
+
+    return numbers[0] - previous_child;
+}
+
+fn part1(input: &str) -> i32 {
+    let mut sum = 0;
+    for line in input.lines() {
+        sum += next_number(
+            line.split_whitespace()
+                .map(|e| e.parse::<i32>().unwrap())
+                .collect(),
+        )
+    }
+    sum
+}
+
+fn part2(input: &str) -> i32 {
+    let mut sum = 0;
+    for line in input.lines() {
+        sum += previous_number(
+            line.split_whitespace()
+                .map(|e| e.parse::<i32>().unwrap())
+                .collect(),
+        );
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -32,6 +74,6 @@ mod tests {
     #[test]
     fn check_part2() {
         let result = part2(INPUT);
-        assert_eq!(result, 0)
+        assert_eq!(result, 2)
     }
 }
